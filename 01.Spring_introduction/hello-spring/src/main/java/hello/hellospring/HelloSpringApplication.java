686808adec4bd2ce1150f073299b07cd349b6b5f
ibk01161 <rkxmsdk@naver.com>
2023-08-21 16:59:06 +0900

    test

--- 01.Spring_introduction/hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ 01.Spring_introduction/hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -9,6 +9,7 @@ public class HelloSpringApplication {
 	public static void main(String[] args) {
 		System.out.print("구동시작");
 		SpringApplication.run(HelloSpringApplication.class, args);
+		System.out.print("구동완료");
 	}
 
 }
