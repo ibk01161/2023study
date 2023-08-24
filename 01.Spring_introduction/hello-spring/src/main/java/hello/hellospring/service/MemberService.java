2458e6ead9010b0da3787a6f6a45d0330dbfc15e
ibk01161 <rkxmsdk@naver.com>
2023-08-24 18:24:46 +0900

    Add Part1_5

---
+++ 01.Spring_introduction/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    // 회원 repository
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 중복 회원 검증
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // 같이 이름이 있는 중복 회원 X
+        memberRepository.findByName(member.getName())        // Optional 안에 객체가 있음
+                .ifPresent(m -> {                            // 객체의 값이 있으면 이 로직을 동작
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 로직
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
