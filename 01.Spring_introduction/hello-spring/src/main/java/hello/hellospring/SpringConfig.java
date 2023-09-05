4499f76c623f854a1e8a015127eac17d1d1192bd
ibk01161 <rkxmsdk@naver.com>
2023-09-05 23:56:22 +0900

    Add Part1_12

--- 01.Spring_introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 01.Spring_introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,22 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    
+    // 2023.09.05, 6-2 DataSource는 스프링이 제공
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     // 스프링이 뜰때 @Configuration을 읽고, memberService 메소드의 로직을 호출을 해서 스프링 빈에 등록해줌
     @Bean
@@ -17,7 +26,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        // 2023.09.05, 6-2 JdbcMemberRepository를 스프링 빈으로 등록하게 변경
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
