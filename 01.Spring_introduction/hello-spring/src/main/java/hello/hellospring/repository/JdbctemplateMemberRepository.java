1feeb5f16b7e33d969728023e0bee68f80a8eced
ibk01161 <rkxmsdk@naver.com>
2023-09-10 23:57:17 +0900

    Add Part1_14

---
+++ 01.Spring_introduction/hello-spring/src/main/java/hello/hellospring/repository/JdbctemplateMemberRepository.java
@@ -0,0 +1,49 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbctemplateMemberRepository implements MemberRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    // 생성자가 1개만 있으면 스프링 빈으로 등록되어 @Autowired를 생략할 수 있음
+    public JdbctemplateMemberRepository(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper());
+        return result.stream().findAny();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+
+    private RowMapper<Member> memberRowMapper() {
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setName("name");
+            return member;
+        };
+    }
+}
