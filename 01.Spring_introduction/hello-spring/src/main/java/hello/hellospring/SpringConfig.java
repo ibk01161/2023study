5b6c27cae5a6eabcfd1445bee667379c58c54646
ibk01161 <rkxmsdk@naver.com>
2023-09-11 23:47:26 +0900

    Add Part1_16

--- 01.Spring_introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 01.Spring_introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,31 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbctemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-    
+
+    /*
     // 2023.09.05, 6-2 DataSource는 스프링이 제공
     private DataSource dataSource;
 
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+     */
+
+    // 2023.09.11 JPA 선언
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     // 스프링이 뜰때 @Configuration을 읽고, memberService 메소드의 로직을 호출을 해서 스프링 빈에 등록해줌
@@ -30,7 +40,9 @@ public class SpringConfig {
         // 2023.09.05, 6-2 JdbcMemberRepository를 스프링 빈으로 등록하게 변경
         //return new JdbcMemberRepository(dataSource);
         // 2023.09.11, 6-4 JdbcTemplate을 스프링 빈으로 등록하게 변경
-        return new JdbctemplateMemberRepository(dataSource);
+        //return new JdbctemplateMemberRepository(dataSource);
+        // 2023.09.11, 6-5 JpaMemberRepository를 스프링 빈으로 등록하게 변경
+        return new JpaMemberRepository(em);
     }
 
 }
--- 01.Spring_introduction/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ 01.Spring_introduction/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,14 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity                         // JPA가 관리하는 엔티티
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)         // PK 매핑
     private Long id;            // 시스템이 저장하는 id (시퀀스)
     private String name;
 
