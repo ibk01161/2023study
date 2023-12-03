f3cdc8a9c42493b11f5289f812e80bbe6b959f16
ibk01161 <rkxmsdk@naver.com>
2023-12-04 01:33:23 +0900

    commit test2

--- Backend_starting_in_one_day/review/src/main/java/com/hoon/review/ReviewApplication.java
+++ Backend_starting_in_one_day/review/src/main/java/com/hoon/review/ReviewApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReviewApplication {
 
 	public static void main(String[] args) {
-		// main
+		// main test
 		SpringApplication.run(ReviewApplication.class, args);
 	}
 
